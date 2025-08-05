138b4f1e5b01caf4d846b69dc84da75bb99c9cee
celina-coding <celinafeghoul@gmail.com>
2025-08-05 19:45:43 +0200

    fetching a random meme from memes API

--- memegenerator/components/Main.jsx
+++ memegenerator/components/Main.jsx
@@ -27,23 +27,6 @@ export default function Main() {
         }))
       
     }
-   
-    /**
-     * Challenge:
-     * Get an array of memes from the imgflip API as soon as
-     * this component renders for the first time.
-     * Check the imgflip documentation for the correct URL.
-     * Save the array of memes (not the whole response
-     * data) to state. (For this app, we'll randomly choose
-     * one of the memes from this array when the user clicks
-     * the "Get a new meme image" button, but we'll do that in
-     * a separate challenge.)
-     * 
-     * Hint: for now, don't try to use an async/await function.
-     * Instead, use `.then()` to resolve the promises
-     * from using `fetch`. We'll learn why after this challenge.
-     */
-    
     
     
     function handleChange(event) {
