af6b12e2ff50433b76ad12b3a04201e7eb4c283b
celina-coding <celinafeghoul@gmail.com>
2025-08-05 19:27:10 +0200

    fetching memes and getting a random one

--- memegenerator/components/Main.jsx
+++ memegenerator/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Main() {
     const [meme, setMeme] = useState({
@@ -6,7 +6,45 @@ export default function Main() {
         bottomText: "Walk into Mordor",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
+    
+    const [memesArray, setMmemesArray] = useState([])
+    
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+         .then(res => res.json())
+         .then(data =>  setMmemesArray(data.data.memes) )
+    }, [])
+    
+    console.log(memesArray)
+    
+    function handleFlipMeme(){
+        console.log("meme flipped")
+        const randomMeme = memesArray[Math.floor(Math.random()*memesArray.length)];
+        console.log(randomMeme.url)
+          setMeme(prevMeme => ({
+            ...prevMeme,
+            imageUrl:randomMeme.url
+        }))
       
+    }
+   
+    /**
+     * Challenge:
+     * Get an array of memes from the imgflip API as soon as
+     * this component renders for the first time.
+     * Check the imgflip documentation for the correct URL.
+     * Save the array of memes (not the whole response
+     * data) to state. (For this app, we'll randomly choose
+     * one of the memes from this array when the user clicks
+     * the "Get a new meme image" button, but we'll do that in
+     * a separate challenge.)
+     * 
+     * Hint: for now, don't try to use an async/await function.
+     * Instead, use `.then()` to resolve the promises
+     * from using `fetch`. We'll learn why after this challenge.
+     */
+    
+    
     
     function handleChange(event) {
         const {value, name} = event.currentTarget
@@ -38,7 +76,7 @@ export default function Main() {
                         value={meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={handleFlipMeme}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src={meme.imageUrl} />
